1ead9e4f29532225ddd74167c1d00475c6949f46
Helen Stosky <poluskyh@gmail.com>
2024-06-25 09:21:35 -0300

    Entrega Helen Stosky Poluboiarinov - 337438

--- entrega/entrega.sh
+++ entrega/entrega.sh
@@ -1,2 +1,36 @@
-# Entrega Nombre Apellido - NumeroDeEstudiante
+# Entrega Helen Stosky Poluboiarinov - Numero Estudiante: 337438
+#!/bin/bash
+mostrar_menu() {
+    echo "1. Opción 1"
+    echo "2. Opción 2"
+    echo "3. Opción 3"
+    echo "4. Salir"
+}
 
+manejar_opcion() {
+    case $1 in
+        1)
+            echo "Has seleccionado la Opción 1"
+            ;;
+        2)
+            echo "Has seleccionado la Opción 2"
+            ;;
+        3)
+            echo "Has seleccionado la Opción 3"
+            ;;
+        4)
+            echo "Saliendo..."
+            exit 0
+            ;;
+        *)
+            echo "Opción inválida. Por favor, selecciona una opción válida."
+            ;;
+    esac
+}
+
+while true; do
+    mostrar_menu
+    read -p "Selecciona una opción: " opcion
+    manejar_opcion $opcion
+    echo " "
+done
